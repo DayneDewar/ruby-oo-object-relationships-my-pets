7f7faaac4dae6dd2cd5cd6ae555fb4a5e00a8013
Dayne Dewar <daynedewar@yahoo.com>
2021-01-14 18:00:32 -0500

    Halfway there

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,18 @@
+require_relative 'owner.rb'
 class Cat
-  # Hi Andrew!
+  @@all = []
+  attr_reader :name
+  attr_accessor :owner, :mood
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,57 @@
+require 'pry'
+require_relative 'cat.rb'
+require_relative 'dog.rb'
+
 class Owner
-  # code goes here
+@@all = []
+  attr_reader :name, :species
+  attr_accessor :reset_all
+  def initialize(name)
+    @species = "human"
+    @name = name
+    @@all << self
+    @reset_all = reset_all
+  end
+
+  def say_species
+    "I am a human."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def cats
+    Cat.all.select {|cat| cat.owner == self}
+  end
+
+  def dogs
+    Dog.all.select {|dog| dog.owner == self}
+  end
+
+  def buy_cat(cat)
+    Cat.new(cat, self)
+  end
+  
+  def buy_dog(dog)
+    Dog.new(dog, self)
+  end
+
+  def walk_dogs
+    self.dogs.each
+    binding.pry
+  end
+
+
 end
+
+
+  
